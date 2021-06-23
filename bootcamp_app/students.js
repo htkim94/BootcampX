d5e057729d8d9b839b110b31c0052736552d4c66
Hotae Kim <hotae.kim.94@gmail.com>
2021-06-23 23:45:45 +0000

    parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const input = process.argv.slice(2);
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+const values = [`%${cohortName}%`, limit];
 
 pool.query(`
 SELECT students.id, students.name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${input[0]}%'
-LIMIT ${input[1]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,8 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const input = process.argv.slice(2);
+const input = process.argv[2];
+const values = [`${input}`];
 
 pool.query(`
   SELECT DISTINCT cohorts.name as cohort, teachers.name as name
@@ -15,10 +16,10 @@ pool.query(`
   JOIN teachers ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${input[0]}'
+  WHERE cohorts.name = $1
   AND completed_at IS NOT NULL
   ORDER BY teachers.name
-`).then(res => {
+`, values).then(res => {
   console.log('connected');
   res.rows.forEach(e => {
     console.log(`${e.cohort}: ${e.name}`);
